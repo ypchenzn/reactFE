5537ffeff8c6552aba0ca1332f16021e69b5c6ea
garynaruto <garynaruto@gmail.com>
2025-04-06 18:09:51 +0800

    fix build path

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,19 @@ import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
+    }
+  },
+  build: {
+    sourcemap: true,
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
     }
   }
 })
